fee4565728b2feac2e1b62b066006217314f641a
Boris Janevski <Boris.Janevski@netcetera.com>
2024-06-08 21:10:03 +0200

    added home viewmodel

--- app/src/main/java/com/boryans/deadline/navigation/NavigationGraph.kt
+++ app/src/main/java/com/boryans/deadline/navigation/NavigationGraph.kt
@@ -1,15 +1,18 @@
 package com.boryans.deadline.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.boryans.deadline.ui.screen.adddeadline.addDeadlineScreen
-import com.boryans.deadline.ui.screen.details.deadlineDetailsScreen
-import com.boryans.deadline.ui.screen.home.homeScreen
 import com.boryans.deadline.ui.screen.adddeadline.navigateToAddDeadlineScreen
+import com.boryans.deadline.ui.screen.details.deadlineDetailsScreen
 import com.boryans.deadline.ui.screen.details.navigateToDeadlineDetails
+import com.boryans.deadline.ui.screen.home.homeScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DeadlineNavigationGraph(
   navController: NavHostController = rememberNavController(),
@@ -17,7 +20,7 @@ fun DeadlineNavigationGraph(
   NavHost(navController = navController, startDestination = Route.Home) {
     homeScreen(
       onNavigateToAddDeadline = { navController.navigateToAddDeadlineScreen() },
-      onNavigateToDeadlineDetails = {navController.navigateToDeadlineDetails("12")}
+      onNavigateToDeadlineDetails = { navController.navigateToDeadlineDetails("12") }
     )
     addDeadlineScreen()
     deadlineDetailsScreen()
