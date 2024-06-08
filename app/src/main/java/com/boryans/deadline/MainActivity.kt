287e311170fcebb875518ac750152fa27ccdc7d7
Boris Janevski <Boris.Janevski@netcetera.com>
2024-06-08 21:11:04 +0200

    added annotation

--- app/src/main/java/com/boryans/deadline/MainActivity.kt
+++ app/src/main/java/com/boryans/deadline/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.boryans.deadline
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.boryans.deadline.navigation.DeadlineNavigationGraph
 import com.boryans.deadline.ui.theme.DeadlineTheme
 
 class MainActivity : ComponentActivity() {
+  @RequiresApi(Build.VERSION_CODES.O)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
